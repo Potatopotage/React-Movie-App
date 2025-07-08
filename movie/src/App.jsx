ebe97a40366555312a1969b9d7842de44e171787
potato_potage <f.potato.potage@gmail.com>
2025-07-09 08:30:17 +0900

    0709_1

--- movie/src/App.jsx
+++ movie/src/App.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Movie  from './Movie'
 
 
 function App() {
-  const [toDo, setToDo] = useState('')
-  const [toDos, setToDos] = useState([])
 
-  const onChange = (event) => setToDo(event.target.value)
-  const onSubmit = (event) => {
-    event.preventDefault()
-    if (toDo === '') {
-      return
-    }
-    setToDos(currentArray => [toDo, ...currentArray])
-    setToDo('')
+  const [loading, setLoading] = useState(true)
+  const [movies, setMovies] = useState([])
+  
+  const getMovies = async() => {
+    const json = await(
+      await fetch(
+        'https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year'
+      )
+    ).json()
+    setMovies(json.data.movies)
+    setLoading(false)
   }
-  console.log(toDos)
+  useEffect(() => {
+    getMovies()
+  }, [])
+  console.log(movies)
 
   return (
     <>
-      <h1>My todos({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input 
-          value={toDo} 
-          type="text" 
-          placeholder='Write your Todo!' 
-          onChange={onChange}
+      {loading ? 
+      <h1>Loading...</h1> : 
+      movies.map(((movie) => 
+        <Movie
+          key={movie.id}
+          medium_cover_image={movie.medium_cover_image}
+          title={movie.title}
+          summary={movie.summary}
+          genres={movie.genres}
         />
-        <button>Add To Do</button>
-      </form>
+
+      ))}
     </>
   )
 }
